b7bea8e7277fbf8f85e99aa2aa87ffd757e044b9
yuka <yury.kalinin@outlook.de>
2023-08-17 18:04:28 +0200

    record sound until i send stop

--- src/main/java/com/sound/recognition/Main.java
+++ src/main/java/com/sound/recognition/Main.java
@@ -1,24 +1,57 @@
 package com.sound.recognition;
 
 import com.sound.recognition.di.ServiceContainer;
-import com.sound.recognition.entities.AudioRecording;
 import com.sound.recognition.exceptions.SoundRecordingException;
+
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) {
         ServiceContainer container = new ServiceContainer();
+        Scanner scanner = new Scanner(System.in);
+        AtomicInteger fileCounter = new AtomicInteger(1);
 
-        String filePath = "aufnahme.wav";
+        AtomicBoolean isRecording = new AtomicBoolean(true);
 
-        // Use the audio recorder to record and save the audio
-        try {
-            container.getAudioRecorder().recordAndSave(filePath);
-        } catch (SoundRecordingException e) {
-           e.printStackTrace();
+        System.out.println("Recording started... Type 'stop' and press Enter to stop recording.");
+
+        Thread recordingThread = new Thread(() -> {
+            try {
+                while (isRecording.get()) {
+                    String filePath = "aufnahme_" + fileCounter.getAndIncrement() + ".wav";
+                    try {
+                        container.getAudioRecorder().recordAndSave(filePath);
+                        System.out.println("Recording saved at: " + filePath);
+                    } catch (SoundRecordingException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (ThreadDeath td) {
+                throw td;  // Rethrow ThreadDeath
+            }
+        });
+
+        recordingThread.start();
+
+        // Wait for the user to type "stop"
+        while (true) {
+            String input = scanner.nextLine();
+            if ("stop".equalsIgnoreCase(input)) {
+                isRecording.set(false);
+                try {
+                    recordingThread.join(); // Wait for the recording thread to finish
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt(); // Restore the interrupted status
+                    break;
+                }
+                break;
+            } else {
+                System.out.println("Invalid command. Type 'stop' to end recording.");
+            }
         }
 
-        // Use the use case to handle business logic and persist the audio data
-        AudioRecording recording = container.getRecordAudio().execute(filePath);
-        System.out.println("Recording saved at: " + recording.getFilePath());
+        System.out.println("Recording stopped.");
     }
 }
